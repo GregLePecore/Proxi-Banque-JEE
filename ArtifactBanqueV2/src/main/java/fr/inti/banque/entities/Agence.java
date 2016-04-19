73bdcf79e184c0736895d270f2fd2dad2bba8766
inti0299 <inti0299@DESKTOP-1QO8TIR>
2016-04-19 11:18:41 +0200

    Suppression de Test, ajout d'Agence

---
+++ ArtifactBanqueV2/src/main/java/fr/inti/banque/entities/Agence.java
@@ -0,0 +1,6 @@
+package fr.inti.banque.entities;
+
+//Tamère
+public class Agence {
+
+}
--- ArtifactBanqueV2/src/main/java/fr/inti/classes/test/TestTest.java
+++
@@ -1,5 +0,0 @@
-package fr.inti.classes.test;
-
-//Ceci est une classe. Pouce bleu si tu m'aimes
-public class TestTest {
-}
