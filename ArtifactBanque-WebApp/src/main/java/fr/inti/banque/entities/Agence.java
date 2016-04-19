87ecab42ce214c7e605c35c7f149f1db50315de2
inti0299 <inti0299@DESKTOP-1QO8TIR>
2016-04-19 12:11:33 +0200

    Commit avant manger : config ajoutée

--- ArtifactBanque-WebApp/src/main/java/fr/inti/banque/entities/Agence.java
+++ ArtifactBanque-WebApp/src/main/java/fr/inti/banque/entities/Agence.java
@@ -1,5 +1,27 @@
 package fr.inti.banque.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+
+@Entity(name = "agence")
+@Table(name = "agences")
 public class Agence {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id_agence")
+	private int id;
+	@Column(name = "numero")
+	private int numero;
+	@Column(name = "prenom")
+	private String prenom;
+	@OneToMany(mappedBy = "personne")
+	private List<Adresse> adresses;
 
 }
