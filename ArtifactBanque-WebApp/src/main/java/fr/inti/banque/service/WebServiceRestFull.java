9133651c3b6cc59469b6907edba8dbe351d5dadb
inti0304 <inti0304@DESKTOP-3NB5CKU>
2016-04-26 10:23:00 +0200

    Ajout de 3 dépendances dans le pom.xml pour accéder au WebService du prof
    Importation du WebService du prof 
    
    Importation du Service-ServiceImpl et ManagedBean du prof faisant appel
    au WebService
    
    Création d'une page Web "Simulation Crédit" pour simuler des crédits de
    consommation.

---
+++ ArtifactBanque-WebApp/src/main/java/fr/inti/banque/service/WebServiceRestFull.java
@@ -0,0 +1,13 @@
+// Fichier du prof
+
+package fr.inti.banque.service;
+
+public interface WebServiceRestFull {
+
+	public  float simulationCreditConsommation(float montant,
+			float duree, float teag);
+
+	public  float simulationCreditConsommation1(float mensualite1,
+			float duree1, float teag1);
+
+}
